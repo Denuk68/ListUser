12003bd319a54916839dfffbc6742e983c38a98b
Dexter <denus.denisovuch@gmail.com>
2020-03-06 23:58:58 +0200

    change itemjs

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -26,28 +26,8 @@ const Item = () => {
                     </div>
                 </div>
             </li>
-
-            <li class="list-group-item">
-                <div class="row w-100">
-                    <div class="col-12 col-sm-6 col-md-3 px-0">
-                        <img src="http://demos.themes.guide/bodeo/assets/images/users/m105.jpg" alt="Seth Frazier" class="img-fluid rounded-circle d-block mx-auto"/>
-                        </div>
-                        <div class="col-12 col-sm-6 col-md-9 text-center text-sm-left">
-                            <span class="name lead">Seth Frazier</span>
-                            <br/>
-                                <span class="fa fa-map-marker fa-fw text-muted" data-toggle="tooltip" title="" data-original-title="7396 E North St"></span>
-                                <span class="text-muted">7396 E North St</span>
-                                <br/>
-                                    <span class="fa fa-phone fa-fw text-muted" data-toggle="tooltip" title="" data-original-title="(560) 180-4143"></span>
-                                    <span class="text-muted small">(560) 180-4143</span>
-                                    <br/>
-                                        <span class="fa fa-envelope fa-fw text-muted" data-toggle="tooltip" title="" data-original-title="seth.frazier@example.com"></span>
-                                        <span class="text-muted small text-truncate">seth.frazier@example.com</span>
-                        </div>
-                    </div>
-                </li>
         </Fragment>
-                        );
-                    };
-                    
+    );
+};
+
 export default Item;
