4ba900b3c9de8af392e125523a41787f874a8d34
Денис Лещук <Leschuk@rivne.itstep.org>
2020-03-09 19:59:25 +0200

    lesson3

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Fragment } from "react";
-import Item from '../Item/Item';
+import Item from './Item/Item';
 import './List.css';
 
 
 const List = () => {
     return (
         <Fragment>
-            <div className="container">
-                <div className="card card-default" id="card_contacts">
-                    <div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
-                        <ul className="list-group pull-down" id="contact-list">
-                            <Item/>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+
+            <ul className="list-group pull-down" id="contact-list">
+                <Item />
+            </ul>
+
         </Fragment>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import List from '../src/Components/List/List';
 import './index.css';
 
 
 
+const App = () => {
+    return (
+        <Fragment>
+            <div className="container">
+                <div  id="card_contacts">
+                    <div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+                        <h1 style={{ textAlign: 'center' }}>Contact List App</h1>
+                        <List />
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
 
-ReactDOM.render(<List />, document.getElementById('root'));
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
